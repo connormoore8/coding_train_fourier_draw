599a105b2fb45f096cf8955027cbd1ed72a03fb8
Connor Moore <connormoore8@gmail.com>
2021-04-15 22:41:53 -0500

    removed superfluos index from contour function

--- Imaging.py
+++ Imaging.py
@@ -2,10 +2,9 @@ class ImageCleaning:
     
     def __init__(self,address):
         self.img = loadImage(address)
+
         
-        
-        
-    def brightscaling(self, thresh):
+    def brightscaling(self,thresh):
         self.img.loadPixels()
         result = createImage(self.img.width, self.img.height, RGB)
         result.loadPixels()
@@ -13,10 +12,7 @@ class ImageCleaning:
             for y in range(self.img.height):
                 index = x + y*self.img.width
                 # print(brightness(self.img.pixels[index]))
-                if (brightness(self.img.pixels[index]) > thresh):
-                    result.pixels[index]  = color(0) # White
-                else:
-                    result.pixels[index]  = color(255)   # Black
+                result.pixels[index] = self.bright(self, index)
         result.updatePixels()
         self.img = result
         
@@ -28,7 +24,20 @@ class ImageCleaning:
             for y in range(self.img.height):
                 index = x + y*self.img.width
                 leftIndex = (x-1) + y*self.img.width
-                result.pixels[index]  = abs(brightness(self.img.pixels[index]) 
-                                            - brightness(self.img.pixels[leftIndex]))
+                result.pixels[index] = self.contour_differential(leftIndex, index)
         result.updatePixels()
         self.img = result
+        
+    def contour_differential(self, Index1, Index2):
+        return abs(brightness(self.img.pixels[Index1]) - brightness(self.img.pixels[Index2]))
+              
+        
+    def bright(self, index):
+        # print(brightness(self.img.pixels[index]))
+        if (brightness(self.img.pixels[index]) > 127):
+            return color(0) # Black
+        else:
+            return color(255)   # White
+        
+    
+    
