1dafe507e9a96614fe614ff30a5665e9452976cf
Connor Moore <connormoore8@gmail.com>
2021-04-22 21:41:41 -0500

    Added an increment that fixed many problems.

--- main.py
+++ main.py
@@ -2,18 +2,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from collections import deque
+# from collections import deque
 import math
 from Complex import Complex
 # from Imaging import ImageCleaning
 import json
 
-# def setup():
-#     path = deque()
-#
-#     # circles = [plt.Circle((0,0), 15, fill=False, ec='y')[0] for x in range(len(fourier))]
-#     #setup screen and list of lines and circles.
-#     return tuple(lines)
+fig = plt.figure(figsize=(10, 10))
+ax = plt.axes(xlim=(-50, 50), ylim=(-50, 50))
+lines = []
+
+
+def setup():
+    for term in fourier:
+        lines.append(ax.plot([], [], color='black', lw=2)[0])
+    # circles = [plt.Circle((0,0), 15, fill=False, ec='y')[0] for x in range(len(fourier))]
+    # setup screen and list of lines and circles.
+    return lines
 
 def dft(x):
     result = []
@@ -52,7 +57,7 @@ def epicycles(lines, circles, time, x, y, phi, series):
         #TODO add circles and line.f
         # set circle where previouse value is.
         # app.ellipse(prevx, prevy, r * 2, r * 2)
-        lines[i].set_data([prevx, x],[prevy, y])
+        lines[i].set_data([0, x],[0, y])
         # set line between two spots.
         # app.line(prevx, prevy, x, y)
         i += 1
@@ -72,17 +77,18 @@ def draw(time):
     i = 0
     for term in fourier:
         # print(lines[0])
-        prevx = x
-        prevy = y
+        prev_x = x
+        prev_y = y
         f = term['frequency']
         p = term['phase']
-        r = 0.15 * term['amplitude']
+        r = 0.25 * term['amplitude']
         x += r * np.cos(f * time + p + phi)
         y += r * np.sin(f * time + p + phi)
         # TODO add circles and line.f
         # set circle where previouse value is.
         # app.ellipse(prevx, prevy, r * 2, r * 2)
-        lines[i].set_data([prevx, x], [prevy, y])
+        lines[i].set_data([prev_x, x], [prev_y, y])
+        i+=1
         # print(lines[i])
     # path.appendleft(v)
     # app.stroke(255)
@@ -100,27 +106,24 @@ def draw(time):
     return lines
 
 def main():
-    fig = plt.figure(figsize=(10, 10))
-    ax = plt.axes(xlim=(-50, 50), ylim=(-50, 50))
+    global fourier
     signal = []
     with open('drawing.txt') as json_file:
         data = json.load(json_file)
-    global fourier
-    global lines
-    global circles
     for i in range(0, len(data['drawing']), 5):
         signal.append(Complex(data['drawing'][i]['x'], data['drawing'][i]['y']))
     fourier = dft(signal)
     fourier = sorted(fourier, key=lambda el: el['amplitude'], reverse=True)
-    lines = [ax.plot([], [], color='black', linewidth=2)[0] for x in range(len(fourier))]
-    print('lines created')
-    plt.style.use('dark_background')
-    print('background loaded')
-    anim = animation.FuncAnimation(fig, draw,
+    # print('lines created')
+    # plt.style.use('dark_background')
+    # print('background loaded')
+    anim = animation.FuncAnimation(fig, draw, init_func = setup,
                                    frames=np.linspace(0,2*np.pi, len(fourier)),
-                                   interval=40,
+                                   interval=20,
                                    blit=True)
 
+    writer = animation.PillowWriter(fps=25)
+    anim.save('lines.gif', writer = writer)
     plt.show()
 
 
