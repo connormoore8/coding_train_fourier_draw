fc4b9964251b6844f24e74b6c4021e384399e192
Connor Moore <connormoore8@gmail.com>
2021-04-27 21:06:48 -0500

    fixed circles, and refactored main setup.

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import matplotlib.animation as animation
 import math
 from Complex import Complex
 import json
-fourier = []
-fig = plt.figure(figsize=(10, 10))
-ax = plt.axes(xlim=(-50, 50), ylim=(-50, 50))
-lines = []
-circles = []
-patches = lines + list(circles)
 
 
 def setup():
-    for i in range(len(fourier)):
-        lines.append(ax.plot([], [], color='y', lw=2)[0])
-        circles.append(plt.Circle((0, 0), 5, fill=False, ec='y'))
-    # circles = [plt.Circle((0,0), 15, fill=False, ec='y')[0] for x in range(len(fourier))]
-    # setup screen and list of lines and circles.
+    for i, line in enumerate(lines):
+        line.set_data([], [])
+        ax.add_patch(line)
+    for i, circle in enumerate(circles):
+        circle.center = (0, 0)
+        circle.radius = 5
+        ax.add_patch(circle)
+
+        # circles[i] = plt.Circle((0, 0), 5, fill=False, ec='y')
+        # ax.add_patch(lines[i])
+        # ax.add_patch(circles[i])
+        # setup screen and list of lines and circles.
     return patches
 
 
@@ -47,8 +48,6 @@ def dft(x):
 
 def draw(time):
     # TODO: reset it update in the animation function.
-    # global wave
-    # app.image(scan.img, 0, 0, app.width, app.height)
     x = 0
     y = 0
     phi = 0
@@ -65,33 +64,45 @@ def draw(time):
         # set circle where previous value is.
         # app.ellipse(prev_x, prev_y, r * 2, r * 2)
         lines[i].set_data([prev_x, x], [prev_y, y])
-        circles[i].set_radius(r)
+        # circles.append(plt.Circle((prev_x, prev_y), r, fill=False, ec='y'))
         circles[i].center = (prev_x, prev_y)
+        circles[i].set_radius(r)
         i += 1
     return patches
 
 
 def main():
     global fourier
+    global ax
+    global lines
+    global circles
+    global patches
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.axes(xlim=(-50, 50), ylim=(-50, 50))
     signal = []
     with open('drawing.txt') as json_file:
         data = json.load(json_file)
-    for i in range(0, len(data['drawing']), 5):
+    for i in range(0, len(data['drawing']), 7):
         signal.append(Complex(data['drawing'][i]['x'], data['drawing'][i]['y']))
     fourier = dft(signal)
     fourier = sorted(fourier, key=lambda el: el['amplitude'], reverse=True)
-    print(len(fourier))
+    N = len(fourier)
+    print("length of Fourier Transform:" + str(N))
+    lines = [plt.plot([], [], 'y', linewidth=2, alpha=0.3)[0] for _ in range(N)]
+    circles = [plt.Circle((0, 0 + i), 0, color='y', alpha=0.3, fill=False) for i in range(N)]
+    patches = lines + circles
     plt.style.use('dark_background')
+    ax.set_facecolor('black')
     print('background loaded')
     anim = animation.FuncAnimation(fig, draw,
                                    init_func=setup,
-                                   frames=np.linspace(0, 2*np.pi, len(fourier)),
-                                   interval=20,
+                                   frames=np.linspace(0, 2 * np.pi, N),
+                                   interval=5,
                                    blit=True,
                                    repeat=False)
     print('generating gif')
-    writer = animation.PillowWriter(fps=25)
-    anim.save('lines.gif', writer=writer)
+    writer = animation.PillowWriter(fps=30)
+    anim.save('lines_circles.gif', writer=writer)
     print('completed gif')
     plt.show()
 
